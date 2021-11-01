c724c0dc5aae0b83628a35a6d2f7e7d89e5a6737
ImranRahimov1995 <imash.odessahouse@gmail.com>
2021-11-01 12:00:54 +0400

    REFACTORING: first-settings

---
+++ project/config/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
