abfba52e551e8ecb3915a85df1b8e30795e54719
imash.odessahouse@gmail.com <imash.odessahouse@gmail.com>
2022-01-09 01:28:38 +0400

    finish

--- project/youtube/forms.py
+++ project/youtube/forms.py
@@ -3,7 +3,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
-REGEX = compile('^https://www.youtu')
+REGEX = compile(r'^https://www.youtu')
+REGEX2 = compile(r'^https://youtu')
 
 class Getlink(forms.Form):
 
@@ -18,7 +19,7 @@ class Getlink(forms.Form):
 
     def clean_link(self):
         data = self.cleaned_data['link']
-        custom_validate = match(REGEX,data)
-        if not custom_validate:
+        custom_validate,custom_validate2 = match(REGEX,data) ,match(REGEX2,data)
+        if not custom_validate or not custom_validate2:
             raise ValidationError("Please enter youtube link")
         return data
