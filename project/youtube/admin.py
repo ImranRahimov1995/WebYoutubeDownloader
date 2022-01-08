c6bc0c86acb5a25066775247a5973874bad4bd89
imash.odessahouse@gmail.com <imash.odessahouse@gmail.com>
2022-01-09 00:24:29 +0400

    Add info about visitors

--- project/youtube/admin.py
+++ project/youtube/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from youtube.models import MyYoutube
+from youtube.models import MyYoutube,Visitor
 
 
 class MyYoutubeAdmin(admin.ModelAdmin):
     list_display = ['title', 'created']
 
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ['user_info', 'visit_time']
+
+
 admin.site.register(MyYoutube,MyYoutubeAdmin)
+admin.site.register(Visitor,VisitorAdmin)
